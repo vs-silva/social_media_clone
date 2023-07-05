f991e004d85cda5faef0783ed47a4b490b8c4916
Vitalino Silva <vitalinosilva@gmail.com>
2023-07-05 17:18:54 +0100

    added port test case

--- server/business/user/__tests__/user.service.spec.ts
+++ server/business/user/__tests__/user.service.spec.ts
@@ -4,6 +4,7 @@ describe('User service tests', () => {
 
     describe('RegisterUser port tests', () => {
 
+        it.todo('RegisterUser port should return null if required fields of RequestUserRegisterDTO are not provided');
         it.todo('RegisterUser port should create a new user and ResponseUserDTO');
 
     });
