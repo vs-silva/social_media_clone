26751cbf2c081deba270ca81ad86d3a818628a12
Vitalino Silva <vitalinosilva@gmail.com>
2023-07-05 17:43:59 +0100

    added RegisterUser port should create a new user and ResponseUserDTO test body

--- server/business/user/__tests__/user.service.spec.ts
+++ server/business/user/__tests__/user.service.spec.ts
@@ -1,11 +1,46 @@
-import {describe, it} from "vitest";
+import {describe, expect, it, vi} from "vitest";
+import {faker} from "@faker-js/faker";
 
 describe('User service tests', () => {
 
+    const idRegex = /\b[0-9a-f]{24}\b/;
+
     describe('RegisterUser port tests', () => {
 
-        it.todo('RegisterUser port should return null if required fields of RequestUserRegisterDTO are not provided');
-        it.todo('RegisterUser port should create a new user and ResponseUserDTO');
+        const fakePassword = faker.internet.password();
+
+        const fakeNewUser: RequestUserRegisterDTO = {
+            email: faker.internet.email(),
+            password: fakePassword,
+            repeatPassword: fakePassword,
+            username: '',
+            name: `${faker.person.firstName()} ${faker.person.lastName()}`
+        };
+
+        it('RegisterUser port should create a new user and ResponseUserDTO', async () => {
+
+            fakeNewUser.username = faker.internet.userName();
+
+            const spy = vi.spyOn(User, 'registerUser');
+            const result = await User.registerUser(fakeNewUser);
+
+            expect(spy).toHaveBeenCalledOnce();
+            expect(spy).toHaveBeenCalledWith(fakeNewUser);
+
+            expect(result).toBeTruthy();
+            expect(result?.id).toBeTruthy();
+            expect(result?.id).toMatch(idRegex);
+
+            expect(result).toStrictEqual(expect.objectContaining(<ResponseUserDTO>{
+                id: expect.any(String),
+                email: expect.any(String),
+                username: expect.any(String),
+                profileImage: expect.any(String),
+                profileCreateDate: expect.any(String),
+                profileLastUpdateDate: expect.any(String),
+                accessToken: expect.any(String)
+            }));
+        });
 
     });
 
