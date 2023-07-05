66e17e5dbdaf77e9f787870e7c3c045d8793ef99
Vitalino Silva <vitalinosilva@gmail.com>
2023-07-05 17:06:35 +0100

    added initial todo test for the user service. RegisterUser port

---
+++ server/business/user/__tests__/user.service.spec.ts
@@ -0,0 +1,11 @@
+import {describe, it} from "vitest";
+
+describe('User service tests', () => {
+
+    describe('RegisterUser port tests', () => {
+
+        it.todo('RegisterUser port should create a new user and ResponseUserDTO');
+
+    });
+
+});
