3f77df63b16380822c82aa03c8d254221b281f2b
Vitalino Silva <vitalinosilva@gmail.com>
2023-07-05 15:33:59 +0100

    added config files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true
+  }
+});
